40adf2556400844853e1f701fa14716361267e75
KamathN593 <nk2017@kamaths.com>
2025-07-22 20:33:32 -0400

    IsValid

---
+++ Stacks_Queues/IsValidParentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        if len(s) % 2 != 0:
+            return False
+        
+        stack = []
+        mapping = {']' : '[', '}' : '{', ')' : '('}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
+    
