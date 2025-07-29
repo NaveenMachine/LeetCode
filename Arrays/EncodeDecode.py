2aee61eed7640f2d532d8244ac815dfdd4935e75
KamathN593 <nk2017@kamaths.com>
2025-07-29 12:22:38 -0400

    endcode and decode

---
+++ Arrays/EncodeDecode.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return ''.join(f'{len(s)}#{s}' for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i = 0
+
+        while i < len(s):
+            j = s.find('#', i)
+            length = int(s[i:j])
+            i = j+1
+            result.append(s[i:i+length])
+            i = length + i
+
+        return result
+    
