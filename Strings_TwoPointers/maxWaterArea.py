bc5b2be5cd598a676d3d1cd90af644d8cd02fd52
KamathN593 <nk2017@kamaths.com>
2025-08-07 17:07:08 -0400

    yo sup

--- Strings_TwoPointers/maxWaterArea.py
+++ Strings_TwoPointers/maxWaterArea.py
@@ -30,6 +30,24 @@ class Solution:
         else:
             return 0
             
+        
+    def betterMaxArea(self, heights: List[int]) -> int:
+        res = 0
+        l, r = 0, len(heights) - 1
+
+        while l < r:
+
+            area = min(heights[l], heights[r]) * (r - 1)
+
+            res = min(res, area)
+
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+        
+        return res
 
 
 
