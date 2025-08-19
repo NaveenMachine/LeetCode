5ebdbc4310e4e2393ad8604ff61480347b18dc0f
KamathN593 <nk2017@kamaths.com>
2025-08-19 17:42:13 -0400

    fix

--- Stacks_Queues/DailyTemps.py
+++ Stacks_Queues/DailyTemps.py
@@ -1,11 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Monotonic Decreasing Stack Solution
+        # Ex: [73, 72, 71]
+        
         res = [0] * len(temperatures)
-        stack = []  # pair: [temp, index]
+        stack = [] # pair: [temp, index]
 
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
                 stackT, stackInd = stack.pop()
-                res[stackInd] = i - stackInd
-            stack.append((t, i))
+                res[stackInd] = (i - stackInd)
+            stack.append([temp, i])
         return res
